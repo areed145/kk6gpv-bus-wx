4f47846e04439afac9f56b236913c5b8456cd61d
areed145 <areed145@gmail.com>
2020-08-25 23:46:18 -0500

    switch to proper use of aysnc

--- bus_wx/bus_wx.py
+++ bus_wx/bus_wx.py
@@ -2,7 +2,7 @@ import paho.mqtt.client as mqtt
 from datetime import datetime
 import json
 import asyncio
-import websockets
+from websockets import connect
 import numpy as np
 import time
 import sys
@@ -11,179 +11,211 @@ import logging
 logging.basicConfig(level=logging.INFO)
 
 
-class BusWx:
-    """Class that listens for new weather station observations"""
+class WxWebSocket:
+    """Class for the weather station websocket"""
 
-    def __init__(self, station_id, device_id, rapid_id, api_key):
-        self.station_id = station_id
-        self.device_id = device_id
-        self.rapid_id = rapid_id
-        self.api_key = api_key
-        self.fail_count = 0
-        self.fail_max = 30
-        self.run()
+    async def __aenter__(self):
+        """Initial websocket connection to weatherstation"""
+        self.station_id = "2098388936"
+        self.device_id = "54051"
+        self.rapid_id = "54053"
+        self.api_key = "20c70eae-e62f-4d3b-b3a4-8586e90f3ac8"
+        self.uri = (
+            "wss://ws.weatherflow.com/swd/data?" + "api_key=" + self.api_key
+        )
 
-    async def wx_connect(self, ws):
+        self.bus_client = mqtt.Client(
+            client_id="kk6gpv-bus-wx", clean_session=False
+        )
+        self.bus_client.connect("broker.mqttdashboard.com", 1883)
+
+        self._conn = connect(self.uri)
+        self.websocket = await self._conn.__aenter__()
+        return self
+
+    async def __aexit__(self, *args, **kwargs):
+        """Exits weather station connection"""
+        await self._conn.__aexit__(*args, **kwargs)
+
+    async def wx_connect(self):
         """Connects to weatherstation"""
-        await ws.send(
+        await self.websocket.send(
             '{"type":"listen_start", "device_id":'
             + self.device_id
             + ', "id":"'
             + self.station_id
             + '"}'
         )
-        await ws.send(
+        await self.websocket.send(
             '{"type":"listen_start", "device_id":'
             + self.rapid_id
             + ', "id":"'
             + self.station_id
             + '"}'
         )
-        await ws.send(
+        await self.websocket.send(
             '{"type":"listen_rapid_start", "device_id":'
             + self.rapid_id
             + ', "id":"'
             + self.station_id
             + '"}'
         )
+        logging.info("connecting...")
 
-    async def wx_on_message(self, ws):
+    async def wx_on_message(self):
         """Publishes each weatherstation observation to MQTT bus"""
         while True:
-            message = await ws.recv()
+            message = await self.websocket.recv()
             message = json.loads(message)
-            if message["type"] == "evt_precip":
-                msg = {}
-                msg["type"] = "wx_precip"
-                msg["timestamp"] = datetime.utcnow().isoformat()
-                try:
-                    self.client.publish(
-                        "kk6gpv_bus/wx/precip", json.dumps(msg), retain=True
-                    )
-                    logging.info(msg)
-                except Exception:
-                    pass
-
-            if message["type"] == "evt_strike":
-                msg = {}
-                msg["type"] = "wx_strike"
-                msg["timestamp"] = datetime.utcnow().isoformat()
-                msg["distance"] = message["evt"][1]
-                msg["energy"] = message["evt"][2]
-                try:
-                    self.client.publish(
-                        "kk6gpv_bus/wx/strike", json.dumps(msg), retain=True
-                    )
-                    logging.info(msg)
-                except Exception:
-                    pass
-
-            if message["type"] == "device_status":
-                msg = {}
-                msg["type"] = "wx_status"
-                msg["voltage"] = message["voltage"]
-                try:
-                    self.client.publish(
-                        "kk6gpv_bus/wx/status", json.dumps(msg), retain=True
-                    )
-                    logging.info(msg)
-                except Exception:
-                    pass
-
-            if message["type"] == "obs_air":
-                msg = {}
-                msg["type"] = "wx_air"
-                msg["timestamp"] = datetime.utcnow().isoformat()
-                msg["temp_f"] = str(
-                    np.round((message["obs"][0][2] * (9 / 5) + 32), 2)
-                )
-                msg["dewpoint_f"] = str(
-                    np.round(
-                        (
-                            message["obs"][0][2]
-                            - (100 - message["obs"][0][3]) / 5
+            if "type" not in message:
+                logging.error(message)
+            else:
+                if message["type"] == "evt_precip":
+                    msg = {}
+                    msg["type"] = "wx_precip"
+                    msg["timestamp"] = datetime.utcnow().isoformat()
+                    try:
+                        self.bus_client.publish(
+                            "kk6gpv_bus/wx/precip",
+                            json.dumps(msg),
+                            retain=True,
                         )
-                        * (9 / 5)
-                        + 32,
-                        2,
-                    )
-                )
-                msg["relative_humidity"] = str(
-                    np.round(message["obs"][0][3], 2)
-                )
-                msg["pressure_in"] = str(
-                    np.round(message["obs"][0][1] * 0.029693, 3)
-                )
-                msg["pressure_trend"] = str(
-                    message["summary"]["pressure_trend"]
-                )
-                msg["strike_count_3h"] = str(
-                    message["summary"]["strike_count_3h"]
-                )
-                msg["strike_last_dist"] = str(
-                    message["summary"]["strike_last_dist"]
-                )
-                msg["strike_last_epoch"] = str(
-                    message["summary"]["strike_last_epoch"]
-                )
-                msg["feels_like"] = str(message["summary"]["feels_like"])
-                msg["heat_index"] = str(message["summary"]["heat_index"])
-                msg["wind_chill"] = str(message["summary"]["wind_chill"])
-                try:
-                    self.client.publish(
-                        "kk6gpv_bus/wx/air", json.dumps(msg), retain=True
-                    )
-                    logging.info(msg)
-                except Exception:
-                    pass
+                        logging.info(msg)
+                    except Exception:
+                        logging.warning(msg)
 
-            if message["type"] == "obs_sky":
-                msg = {}
-                msg["type"] = "wx_sky"
-                msg["timestamp"] = datetime.utcnow().isoformat()
-                msg["wind_degrees"] = str(message["obs"][0][7])
-                msg["wind_mph"] = str(
-                    np.round(message["obs"][0][5] * 1.94384, 2)
-                )
-                msg["wind_gust_mph"] = str(
-                    np.round(message["obs"][0][6] * 1.94384, 2)
-                )
-                msg["precip_today_in"] = str(
-                    np.round(message["obs"][0][11] * 0.0393701, 3)
-                )
-                msg["solar_radiation"] = str(message["obs"][0][10])
-                msg["uv"] = str(message["obs"][0][2])
-                msg["wind_degrees"] = str(message["obs"][0][7])
-                try:
-                    self.client.publish(
-                        "kk6gpv_bus/wx/sky", json.dumps(msg), retain=True
-                    )
-                    logging.info(msg)
-                except Exception:
-                    pass
+                if message["type"] == "evt_strike":
+                    msg = {}
+                    msg["type"] = "wx_strike"
+                    msg["timestamp"] = datetime.utcnow().isoformat()
+                    msg["distance"] = message["evt"][1]
+                    msg["energy"] = message["evt"][2]
+                    try:
+                        self.bus_client.publish(
+                            "kk6gpv_bus/wx/strike",
+                            json.dumps(msg),
+                            retain=True,
+                        )
+                        logging.info(msg)
+                    except Exception:
+                        logging.warning(msg)
 
-            if message["type"] == "rapid_wind":
-                msg = {}
-                msg["type"] = "wx_wind"
-                msg["timestamp"] = datetime.utcnow().isoformat()
-                msg["wind_degrees"] = str(message["ob"][2])
-                msg["wind_mph"] = str(np.round(message["ob"][1] * 1.94384, 2))
-                try:
-                    self.client.publish(
-                        "kk6gpv_bus/wx/wind", json.dumps(msg), retain=True
-                    )
-                    logging.info(msg)
-                except Exception:
-                    pass
+                if message["type"] == "device_status":
+                    msg = {}
+                    msg["type"] = "wx_status"
+                    msg["voltage"] = message["voltage"]
+                    try:
+                        self.bus_client.publish(
+                            "kk6gpv_bus/wx/status",
+                            json.dumps(msg),
+                            retain=True,
+                        )
+                        logging.info(msg)
+                    except Exception:
+                        logging.warning(msg)
 
-    async def weatherstation(self):
-        """Initial websocket connection to weatherstation"""
-        uri = "wss://ws.weatherflow.com/swd/data?" + "api_key=" + self.api_key
-        async with websockets.connect(uri) as ws:
-            await self.wx_connect(ws)
-            await self.wx_on_message(ws)
+                if message["type"] == "obs_air":
+                    msg = {}
+                    msg["type"] = "wx_air"
+                    msg["timestamp"] = datetime.utcnow().isoformat()
+                    msg["temp_f"] = str(
+                        np.round((message["obs"][0][2] * (9 / 5) + 32), 2)
+                    )
+                    msg["dewpoint_f"] = str(
+                        np.round(
+                            (
+                                message["obs"][0][2]
+                                - (100 - message["obs"][0][3]) / 5
+                            )
+                            * (9 / 5)
+                            + 32,
+                            2,
+                        )
+                    )
+                    msg["relative_humidity"] = str(
+                        np.round(message["obs"][0][3], 2)
+                    )
+                    msg["pressure_in"] = str(
+                        np.round(message["obs"][0][1] * 0.029693, 3)
+                    )
+                    msg["pressure_trend"] = str(
+                        message["summary"]["pressure_trend"]
+                    )
+                    msg["strike_count_3h"] = str(
+                        message["summary"]["strike_count_3h"]
+                    )
+                    msg["strike_last_dist"] = str(
+                        message["summary"]["strike_last_dist"]
+                    )
+                    msg["strike_last_epoch"] = str(
+                        message["summary"]["strike_last_epoch"]
+                    )
+                    msg["feels_like"] = str(message["summary"]["feels_like"])
+                    msg["heat_index"] = str(message["summary"]["heat_index"])
+                    msg["wind_chill"] = str(message["summary"]["wind_chill"])
+                    try:
+                        self.bus_client.publish(
+                            "kk6gpv_bus/wx/air", json.dumps(msg), retain=True
+                        )
+                        logging.info(msg)
+                    except Exception:
+                        logging.warning(msg)
+
+                if message["type"] == "obs_sky":
+                    msg = {}
+                    msg["type"] = "wx_sky"
+                    msg["timestamp"] = datetime.utcnow().isoformat()
+                    msg["wind_degrees"] = str(message["obs"][0][7])
+                    msg["wind_mph"] = str(
+                        np.round(message["obs"][0][5] * 1.94384, 2)
+                    )
+                    msg["wind_gust_mph"] = str(
+                        np.round(message["obs"][0][6] * 1.94384, 2)
+                    )
+                    msg["precip_today_in"] = str(
+                        np.round(message["obs"][0][11] * 0.0393701, 3)
+                    )
+                    msg["solar_radiation"] = str(message["obs"][0][10])
+                    msg["uv"] = str(message["obs"][0][2])
+                    msg["wind_degrees"] = str(message["obs"][0][7])
+                    try:
+                        self.bus_client.publish(
+                            "kk6gpv_bus/wx/sky", json.dumps(msg), retain=True
+                        )
+                        logging.info(msg)
+                    except Exception:
+                        logging.warning(msg)
+
+                if message["type"] == "rapid_wind":
+                    msg = {}
+                    msg["type"] = "wx_wind"
+                    msg["timestamp"] = datetime.utcnow().isoformat()
+                    msg["wind_degrees"] = str(message["ob"][2])
+                    msg["wind_mph"] = str(
+                        np.round(message["ob"][1] * 1.94384, 2)
+                    )
+                    try:
+                        self.bus_client.publish(
+                            "kk6gpv_bus/wx/wind", json.dumps(msg), retain=True
+                        )
+                        logging.info(msg)
+                    except Exception:
+                        logging.warning(msg)
+
+
+class BusWx:
+    """Class that listens for new weather station observations"""
+
+    def __init__(self):
+        """Initialize class"""
+        self.fail_count = 0
+        self.fail_max = 30
+        self.wws = WxWebSocket()
+        self.loop = asyncio.get_event_loop()
+        self.run()
 
     def fail_check(self):
+        """Check for failure count"""
         self.fail_count += 1
         logging.warning(
             "couldn't connect {0} time(s)".format(str(self.fail_count))
@@ -193,26 +225,21 @@ class BusWx:
             sys.exit(1)
 
     def run(self):
-        """Async loop running the function"""
+        """Run the bus"""
         while True:
             try:
-                self.client = mqtt.Client(
-                    client_id="kk6gpv-bus-wx", clean_session=False
-                )
-                self.client.connect("broker.mqttdashboard.com", 1883)
-                asyncio.get_event_loop().run_until_complete(
-                    self.weatherstation()
-                )
+                self.loop.run_until_complete(self.__async__run())
                 self.fail_count = 0
             except Exception:
                 time.sleep(2)
                 self.fail_check()
 
+    async def __async__run(self):
+        """async run"""
+        async with self.wws as ws:
+            await ws.wx_connect()
+            await ws.wx_on_message()
+
 
 if __name__ == "__main__":
-    bus = BusWx(
-        station_id="2098388936",
-        device_id="54051",
-        rapid_id="54053",
-        api_key="20c70eae-e62f-4d3b-b3a4-8586e90f3ac8",
-    )
+    bus = BusWx()
